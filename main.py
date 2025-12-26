d4abe0c58a55588107e6bdd96fb9cf1425d440b9
echelon456 <echezonaemefesi@gmail.com>
2025-12-26 07:56:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -50,4 +50,8 @@ def callback():
     return f"Logged in as {user['username']}#{user['discriminator']}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    import os
+
+port = int(os.environ.get("PORT", 3000))
+
+app.run(host="0.0.0.0", port=port)
